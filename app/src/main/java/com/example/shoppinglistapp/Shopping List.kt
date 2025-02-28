ab6ed8d08c72f2aca209f97ea5a2512304d42686
Master Luke Otieno <lukemclain4@gmail.com>
2025-02-28 23:42:46 +0300

    Setting up the item list

--- app/src/main/java/com/example/shoppinglistapp/Shopping List.kt
+++ app/src/main/java/com/example/shoppinglistapp/Shopping List.kt
@@ -1,15 +1,18 @@
 package com.example.shoppinglistapp
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,6 +22,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 data class ShoppingItem(
@@ -64,11 +68,41 @@ fun ShoppingListApp() {
                 .padding(16.dp)
         ) {
             items(sItems) {
+                ShoppingListItem(it, {}, {})
             }
         }
     }
     if (showDialog) {
-        AlertDialog(onDismissRequest = { showDialog = false }, confirmButton = { /*TODO*/ },
+        AlertDialog(onDismissRequest = { showDialog = false }, confirmButton = {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween /*This puts as much space as possible between the add and the cancel buttons*/
+            ) {
+                Button(onClick = {
+                    if (itemName.isNotBlank()) {
+                        val newItem = ShoppingItem(
+                            id = sItems.size + 1,
+                            name = itemName,
+                            quantity = itemQuantity.toInt()//convert what we enter to int
+                        )
+                        sItems = sItems + newItem
+                        showDialog = false
+                        itemName =
+                            "" //reset the item name to an empty string to avoid manually deleting every time we enter a new shopping item
+                        itemQuantity = ""
+                    }
+                }) {
+                    Text(text = "Add")
+                }
+                Button(onClick = {
+                    showDialog = false
+                }) { //This button deactivates the alert dialog
+                    Text(text = "Cancel")
+                }
+            }
+        },
             title = { Text(text = "Add Shopping Item") },
             text = {
                 Column {
@@ -93,5 +127,24 @@ fun ShoppingListApp() {
                 }
             }
         )
+    }
+}
+
+@Composable
+fun ShoppingListItem(
+    item: ShoppingItem,
+    onEditClick: () -> Unit,
+    onDeleteClick: () -> Unit,
+) {
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .border(
+                border = BorderStroke(3.dp, Color(206, 20, 255)),
+                shape = RoundedCornerShape(20)
+            )
+    ) {
+        Text(text = item.name, modifier = Modifier.padding(8.dp))
     }
 }
